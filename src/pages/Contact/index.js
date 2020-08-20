3467d5fe9fac5c29307c1249fe59e71628eb11a8
Tim Tanner <timtanner@tanner-companies.com>
2020-08-19 23:26:26 -0500

    Styling

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -14,15 +14,15 @@ function Contact() {
                 <div className="col-10">
                     <form>
                         <div className="form-group">
-                            <label for="exampleFormControlInput1">Name</label>
+                            <label>Name</label>
                             <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="First and Last Name"></input>
                         </div>
                         <div className="form-group">
-                            <label for="exampleFormControlInput1">Email address</label>
+                            <label>Email address</label>
                             <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
                         </div>
                         <div className="form-group">
-                            <label for="exampleFormControlTextarea1">Message</label>
+                            <label>Message</label>
                             <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
